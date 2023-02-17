d35bd59c86dbcc0146202dcb45077340f9f2b885
4002-Nonye <royaltysungbin@gmail.com>
2023-02-16 23:40:46 -0800

    removed search functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -39,7 +39,7 @@ const Header = () => {
           onSearch={fetchData}
          
         />
-        <Results  fetchedData={fetchedData}/>
+        <Results/>
       </header>
     </>
   );
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,32 +1,29 @@
 import React, { useState } from "react";
 
-const Results = ({fetchedData}) => {
-  // console.log(fetchedData.location.timezone)
+const Results = () => {
+  
   const [ipAddress, setIpAddress] = useState("192.67.904.99");
   const [location, setLocation] = useState("Brooklyn, NY 10001");
   const [timeZone, setTimeZone] = useState("UTC -5:00");
   const [isp, setISP] = useState("SpaceX Starlink");
-//  const res =fetchedData.map((data)=>{
-  //<div></div>
-
-//  })
+ 
   return (
     <div className="bg-white p-5 py-8 lg:py-0 z-10 w-9/12 lg:h-3/4 flex flex-col lg:flex-row rounded-lg text-center justify-between absolute top-48 md:top-52 drop-shadow-2xl items-center">
       <div className="container ">
         <h3 className="card">IP ADDRESS</h3>
-        <p className="p">{fetchedData.ip}</p>
+        <p className="p">{ipAddress}</p>
       </div>
       <div className="container">
         <h3 className="card">Location</h3>
-          <p className="p">{fetchedData.location.region}, {fetchedData.location.country} {fetchedData.location.geonameId}</p>
+        <p className="p">{location}</p>
       </div>
       <div className="container">
         <h3 className="card">time zone</h3>
-        <p className="p">UTC {fetchedData.location.timezone}</p>
+        <p className="p">{timeZone}</p>
       </div>
       <div className="container border-none">
         <h3 className="card">isp</h3>
-        <p className="p">{fetchedData.isp}</p>
+        <p className="p">{isp}</p>
       </div>
     </div>
   );
